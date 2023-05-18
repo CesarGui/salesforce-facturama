a813a03def73c86f4f3783216d88990daa34569b
CesarGui <cesar.guillermo@xiaexperience.com>
2023-05-18 16:50:27 -0600

    archivo

--- facturación/FacturamaIntegracion.apex.dart
+++ facturación/FacturamaIntegracion.apex.dart
@@ -1,10 +1,10 @@
 public class FacturamaIntegracion {
-   // Variables de configuración
+
    private static final String FACTURAMA_API_URL = 'https://api.facturama.mx/';
    private static final String FACTURAMA_TOKEN = 'TU_TOKEN';
    private static final String FACTURAMA_SECRET = 'TU_SECRET';
 
-   // Método para realizar una llamada a Facturama
+  
    public static HttpResponse callFacturamaAPI(String endpoint, String method, String requestBody) {
       HttpRequest request = new HttpRequest();
       request.setEndpoint(FACTURAMA_API_URL + endpoint);
@@ -17,20 +17,17 @@ public class FacturamaIntegracion {
       return http.send(request);
    }
 
-   // Método de ejemplo para crear una factura en Facturama
    public static void createFactura() {
-      // Construye el cuerpo de la solicitud con los datos de la factura
       String requestBody = '{ "client": { "name": "Cliente de prueba", "taxId": "ABC123" }, "items": [ { "productCode": "P001", "description": "Producto de prueba", "quantity": 1, "price": 100 } ] }';
-      
-      // Realiza la llamada a la API de Facturama para crear la factura
+  
       HttpResponse response = callFacturamaAPI('invoices', 'POST', requestBody);
       
-      // Maneja la respuesta de Facturama (por ejemplo, verifica el código de estado HTTP y procesa la respuesta)
+
       if (response.getStatusCode() == 201) {
-         // Factura creada exitosamente
+
          System.debug('Factura creada con éxito. Respuesta: ' + response.getBody());
       } else {
-         // Error al crear la factura
+
          System.debug('Error al crear la factura. Código de estado: ' + response.getStatusCode() + ', Respuesta: ' + response.getBody());
       }
    }
